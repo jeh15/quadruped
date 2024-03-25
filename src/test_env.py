a0f7fcef8e3b9ff8b43c820bcbd61c8477ade5a9
jeh15 <jeh15@fsu.edu>
2024-03-25 16:48:11 -0400

    Added Unitree environment branch.

--- src/test_env.py
+++ src/test_env.py
@@ -9,33 +9,39 @@ from brax.io import html
 import time
 
 import quadruped
+import unitree
 
 jax.config.update("jax_enable_x64", True)
 np.set_printoptions(precision=4)
 
 
 def main(argv=None):
-    env = quadruped.Quadruped(backend='mjx')
+    # env = quadruped.Quadruped(backend='mjx')
+    env = unitree.Unitree(backend='mjx')
     episode_run_time = 20.0  # Seconds
     batch_run_time = 0.5  # Seconds
     episode_length = int(episode_run_time / env.dt)
+    episode_length = 1
 
-    reset_fn = jax.jit(env.reset)
-    step_fn = jax.jit(env.step)
+    # reset_fn = jax.jit(env.reset)
+    # step_fn = jax.jit(env.step)
+    reset_fn = env.reset
+    step_fn = env.step
 
     # Compile Step Function for timing:
     initial_key = jax.random.key(42)
     state = reset_fn(initial_key)
 
     state_history = [state.pipeline_state]
-    kp = np.array([2.0, 6.0, 2.0, 6.0, 2.0, 6.0, 2.0, 6.0])
+    kp = 1.0
     kd = 0.1
 
     start_time = time.time()
     for i in range(episode_length):
         leg_q = state.pipeline_state.q[7:]
         leg_qd = state.pipeline_state.qd[6:]
-        ctrl_input = kp * (np.zeros_like(leg_q) - leg_q) - kd * leg_qd
+        # ctrl_input = kp * (np.zeros_like(leg_q) - leg_q) - kd * leg_qd
+        ctrl_input = env.base_control
         if i % 100 == 0:
             print(f'Control Input: {ctrl_input}')
         state = step_fn(state, ctrl_input)
