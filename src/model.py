Added observation normalization, action normalization, correct kernal initialization.

src/model.py
@@ -1,3 +1,4 @@
+import jax
 import jax.numpy as jnp
 from flax import linen as nn
 
@@ -9,54 +10,71 @@ class ActorCriticNetwork(nn.Module):
         dtype = jnp.float64
         policy_features = 256
         value_features = 256
+        initializer = jax.nn.initializers.lecun_uniform()
+        policy_layer_initializer = jax.nn.initializers.variance_scaling(
+            scale=0.001,
+            mode='fan_in',
+            distribution='uniform',
+        )
+        value_layer_initializer = jax.nn.initializers.lecun_uniform()
         self.dense_1 = nn.Dense(
             features=policy_features,
             name='dense_1',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_2 = nn.Dense(
             features=policy_features,
             name='dense_2',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_3 = nn.Dense(
             features=policy_features,
             name='dense_3',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_4 = nn.Dense(
             features=policy_features,
             name='dense_4',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_5 = nn.Dense(
             features=value_features,
             name='dense_5',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_6 = nn.Dense(
             features=value_features,
             name='dense_6',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_7 = nn.Dense(
             features=value_features,
             name='dense_7',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.dense_8 = nn.Dense(
             features=value_features,
             name='dense_8',
+            kernel_init=initializer,
             dtype=dtype,
         )
         self.policy_layer = nn.Dense(
-            features=self.action_space,
+            features=2 * self.action_space,
             name='mean_layer',
+            kernel_init=policy_layer_initializer,
             dtype=dtype,
         )
         self.value_layer = nn.Dense(
             features=1,
             name='value_layer',
+            kernel_init=value_layer_initializer,
             dtype=dtype,
         )
 
@@ -81,11 +99,14 @@ class ActorCriticNetwork(nn.Module):
         w = self.dense_8(w)
 
         # Output Layer:
-        # policy_output = self.policy_layer(y)
-        # mean, std = jnp.split(policy_output, 2, axis=-1)
-        # std = nn.softplus(std)
-        mean = self.policy_layer(y)
-        std = 0.1 * jnp.ones_like(mean)
+        policy_output = self.policy_layer(y)
+        mean, std = jnp.split(policy_output, 2, axis=-1)
+        mean = nn.tanh(mean)
+        std = nn.softplus(std)
+
+        # mean = self.policy_layer(y)
+        # std = 0.1 * jnp.ones_like(mean)
+
         values = self.value_layer(w)
         return mean, std, values
 

src/model_utilities.py
@@ -4,6 +4,9 @@ import functools
 import jax
 import jax.numpy as jnp
 import distrax
+from brax.training.acme import running_statistics as rs
+
+import statistics_utilities
 
 # Types:
 from flax.core import FrozenDict
@@ -14,8 +17,12 @@ PRNGKey = jax.Array
 def forward_pass(
     model_params: FrozenDict,
     apply_fn: Callable[..., Any],
+    statistics_state: rs.RunningStatisticsState,
     x: jax.Array,
 ) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    # Normalize Observations:
+    x = statistics_utilities.normalize(statistics_state, x, None)
+    # Pass Normalized Observations:
     mean, std, values = apply_fn({"params": model_params}, x)
     return mean, std, values
 
@@ -73,9 +80,9 @@ def evaluate_action(
 @functools.partial(jax.jit, static_argnames=["episode_length"])
 @functools.partial(jax.vmap, in_axes=(0, 0, 0, None), out_axes=(0, 0))
 def calculate_advantage(
-    rewards: jax.typing.ArrayLike,
-    values: jax.typing.ArrayLike,
-    mask: jax.typing.ArrayLike,
+    rewards: jax.Array,
+    values: jax.Array,
+    mask: jax.Array,
     episode_length: int,
 ) -> Tuple[jnp.ndarray, jnp.ndarray]:
     gamma = 0.99
@@ -94,16 +101,18 @@ def calculate_advantage(
 
 # Vmapped Replay Function:
 @functools.partial(jax.jit, static_argnames=["apply_fn"])
-@functools.partial(jax.vmap, in_axes=(None, None, 1, 1), out_axes=(1, 1, 1, 1, 1))
+@functools.partial(jax.vmap, in_axes=(None, None, None, 1, 1), out_axes=(1, 1, 1, 1, 1))
 def replay(
     model_params: FrozenDict,
     apply_fn: Callable[..., Any],
+    statistics_state: rs.RunningStatisticsState,
     model_input: jax.typing.ArrayLike,
     actions: jax.typing.ArrayLike,
 ) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
     mean, std, values = forward_pass(
         model_params,
         apply_fn,
+        statistics_state,
         model_input,
     )
     log_probability, entropy = evaluate_action(mean, std, actions)
@@ -120,6 +129,7 @@ def replay(
 def loss_function(
     model_params: FrozenDict,
     apply_fn: Callable[..., Any],
+    statistics_state: rs.RunningStatisticsState,
     model_input: jax.typing.ArrayLike,
     actions: jax.typing.ArrayLike,
     advantages: jax.typing.ArrayLike,
@@ -147,6 +157,7 @@ def loss_function(
     values, log_probability, entropy, mean, std = replay(
         model_params,
         apply_fn,
+        statistics_state,
         model_input,
         actions,
     )

src/train.py
@@ -1,5 +1,4 @@
 import os
-import re
 import pickle
 from absl import app, flags
 import time
@@ -10,12 +9,13 @@ import jax.numpy as jnp
 import optax
 from flax.training import train_state
 from brax.envs.wrappers.training import wrap
-import orbax.checkpoint
+from brax.training.acme import running_statistics as rs
 import orbax.checkpoint as ocp
 
 import model
 import model_utilities
 import optimization_utilities
+import statistics_utilities
 import control_utilities
 import checkpoint
 
@@ -24,7 +24,7 @@ import unitree
 jax.config.update("jax_enable_x64", True)
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('filename', None, 'Checkpoint file name.', short_name='f')
+flags.DEFINE_boolean('load_checkpoint', False, 'Load latest checkpoint.', short_name='l')
 
 
 def init_params(module, input_size, key):
@@ -105,29 +105,44 @@ def main(argv=None):
     )
     del initial_params
 
+    # Create Running Statistics:
+    statistics_state = rs.init_state(
+        jnp.zeros((env.observation_size + env.action_size),)
+    )
+
     # Create Checkpoint Manager:
     checkpoint_metadata = checkpoint.default_checkpoint_metadata()
     manager_options = checkpoint.default_checkpoint_options()
-    checkpoint_directory = checkpoint_path = os.path.join(os.path.dirname(__file__), "checkpoints")
+    checkpoint_directory = os.path.join(os.path.dirname(__file__), "checkpoints")
     manager = ocp.CheckpointManager(
         directory=checkpoint_directory,
         options=manager_options,
         item_names=('state', 'metadata'),
     )
 
+    # Extract remap ranges:
+    action_range = jnp.tile(
+        A=jnp.array([-1.0, 1.0]),
+        reps=(env.action_size, 1),
+    )
+    control_range = env.sys.actuator_ctrlrange
+
     iteration_step = 0
-    # Rework:
-    if FLAGS.filename is not None:
-        target = {'model': model_state}
-        orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
-        checkpoint_path = os.path.join(
-            os.path.dirname(__file__), FLAGS.filename,
+    if FLAGS.load_checkpoint is not None:
+        checkpoint_metadata = checkpoint.default_checkpoint_metadata()
+        manager_options = checkpoint.default_checkpoint_options()
+        checkpoint_directory = os.path.join(os.path.dirname(__file__), "checkpoints")
+        manager = ocp.CheckpointManager(
+            directory=checkpoint_directory,
+            options=manager_options,
+            item_names=('state', 'metadata'),
         )
-        model_state = orbax_checkpointer.restore(
-            checkpoint_path, item=target,
-        )['model']
-        # Quick fix for iteration step: (TODO(jeh15): Store iteration in model)
-        iteration_step = int(re.findall(r'\d+', FLAGS.filename)[0])
+        model_state, metadata = checkpoint.load_checkpoint(
+            manager=manager,
+            train_state=model_state,
+            metadata=checkpoint_metadata,
+        )
+        iteration_step = metadata['iteration']
 
     # Learning Loop:
     training_length = 300
@@ -144,7 +159,6 @@ def main(argv=None):
         # reset_key = jnp.zeros((num_envs, 2), dtype=jnp.uint32)
         states = reset_fn(reset_key)
         actions = jnp.zeros((num_envs, env.action_size))
-        control_input = jnp.zeros((num_envs, env.action_size))
         state_history = [states]
         model_input_episode = []
         states_episode = []
@@ -156,15 +170,18 @@ def main(argv=None):
         metrics_episode = []
         mean_episode = []
         std_episode = []
-        episode_start = time.time()
         for environment_step in range(episode_iterator_length):
             for batch_step in range(batch_iterator_length):
                 key, env_key = jax.random.split(env_key)
                 model_input = jnp.concatenate([states.obs, actions], axis=1)
-                # model_input = jnp.concatenate([states.obs, control_input], axis=1)
+                statistics_state = statistics_utilities.update(
+                    state=statistics_state,
+                    x=model_input,
+                )
                 mean, std, values = model_utilities.forward_pass(
                     model_params=model_state.params,
                     apply_fn=model_state.apply_fn,
+                    statistics_state=statistics_state,
                     x=model_input,
                 )
                 actions, log_probability, entropy = model_utilities.select_action(
@@ -172,9 +189,14 @@ def main(argv=None):
                     std=std,
                     key=env_key,
                 )
+                control_input = control_utilities.remap_controller(
+                    actions,
+                    action_range,
+                    control_range,
+                )
                 next_states = step_fn(
                     states,
-                    actions,
+                    control_input,
                 )
                 states_episode.append(states.obs)
                 values_episode.append(jnp.squeeze(values))
@@ -234,6 +256,7 @@ def main(argv=None):
             _, _, values = model_utilities.forward_pass(
                 model_params=model_state.params,
                 apply_fn=model_state.apply_fn,
+                statistics_state=statistics_state,
                 x=model_input,
             )
 
@@ -253,6 +276,7 @@ def main(argv=None):
             # Update Function:
             model_state, loss = optimization_utilities.train_step(
                 model_state,
+                statistics_state,
                 model_input_episode,
                 actions_episode,
                 advantage_episode,