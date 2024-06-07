5365583d2dc53efc482151b0f3a5e29d7095c42f
jeh15 <jeh15@fsu.edu>
2024-06-07 19:52:54 -0400

    Clean up.

--- regress_params.py
+++ regress_params.py
@@ -8,6 +8,7 @@ import jax.numpy as jnp
 import numpy as np
 
 import flax.struct
+import optax
 
 import brax
 from brax.io import mjcf
@@ -16,6 +17,8 @@ from brax.mjx import pipeline
 import matplotlib.pyplot as plt
 
 import time
+
+jax.config.update('jax_enable_x64', True)
 
 
 @flax.struct.dataclass
@@ -41,30 +44,74 @@ def main(argv=None):
     friction_param = sys.dof_damping
     target_param = 0.5239
 
-    with open('params_True.pkl', 'rb') as f:
+    # Initialize the parameter and optimizer:
+    solver = optax.adam(learning_rate=1e-3)
+    params = jnp.array(friction_param)
+    opt_state = solver.init(params)
+
+    with open('train_params.pkl', 'rb') as f:
         [state_history, control_history] = pickle.load(f)
 
     q_measured = list(map(lambda x: x.q, state_history))
     qd_measured = list(map(lambda x: x.qd, state_history))
     q_measured = np.asarray(q_measured)
     qd_measured = np.asarray(qd_measured)
-    control_history = np.asarray(control_history)
+    control_history = np.swapaxes(
+        np.asarray(control_history), 0, 1,
+    )
 
-    # Create a pipeline
-    init_fn = jax.jit(pipeline.init)
-    step_fn = jax.jit(pipeline.step)
+    # Get the number of time steps and trials
+    num_time_steps = q_measured.shape[0]
+    num_trials = q_measured.shape[1]
+    num_dofs = q_measured.shape[2]
+    num_act = control_history.shape[-1]
 
-    # Run initial comparison
-    state = init_fn(sys, q_measured[0], qd_measured[0])
+    # Structure Data into Minibatches:
+    minibatch_size = 10
+    num_batches = num_time_steps // minibatch_size
+    q_batch = np.reshape(
+        q_measured, (-1, minibatch_size, num_trials, num_dofs),
+    )
+    qd_batch = np.reshape(
+        qd_measured, (-1, minibatch_size, num_trials, num_dofs),
+    )
+    ctrl_batch = np.reshape(
+        control_history, (-1, minibatch_size, num_trials, num_act),
+    )
+
+    # Axis order: (batch, trial, minibatch, dof)
+    q_batch = np.swapaxes(q_batch, 1, 2)
+    qd_batch = np.swapaxes(qd_batch, 1, 2)
+    ctrl_batch = np.swapaxes(ctrl_batch, 1, 2)
+
+    # Shuffle Data:
+    key = jax.random.PRNGKey(42)
+    key, subkey = jax.random.split(key)
+
+    # Use same key to preserve order:
+    q_train = jax.random.permutation(subkey, q_batch, axis=0)
+    qd_train = jax.random.permutation(subkey, qd_batch, axis=0)
+    ctrl_train = jax.random.permutation(subkey, ctrl_batch, axis=0)
+
+    # VMAP and Jit the pipeline functions:
+    vmap_init_fn = jax.vmap(pipeline.init, in_axes=(None, 0, 0))
+    init_fn = jax.jit(vmap_init_fn)
+
+    vmap_step_fn = jax.vmap(pipeline.step, in_axes=(None, 0, 0))
+    step_fn = jax.jit(vmap_step_fn)
+
+    # Run initial comparison of random trial:
+    random_idx = jax.random.randint(subkey, (), minval=0, maxval=num_trials-1)
+    q_initial_trial = q_measured[:, random_idx]
+    qd_initial_trial = qd_measured[:, random_idx]
+    ctrl_initial_trial = control_history[:, random_idx]
+    state = jax.jit(pipeline.init)(sys, q_initial_trial[0], qd_initial_trial[0])
     q_history_init = []
     qd_history_init = []
     for i in range(control_history.shape[0]):
-        state = step_fn(sys, state, control_history[i])
+        state = jax.jit(pipeline.step)(sys, state, ctrl_initial_trial[i])
         q_history_init.append(state.q)
         qd_history_init.append(state.qd)
-
-    # init_vmap = jax.vmap(pipeline.init, in_axes=(None, 0, 0))
-    # init_fn = jax.jit(init_vmap)
 
     # Test different loss functions: (Consecutive rollout, Random VMAP rollout)
     # History rollout:
@@ -86,6 +133,10 @@ def main(argv=None):
             xs=batch.ctrl,
             length=minibatch_size,
         )
+
+        # Reshape the data: axis -> (trials, time, dof)
+        q = jnp.swapaxes(q, 0, 1)
+        qd = jnp.swapaxes(qd, 0, 1)
 
         loss = (
             jnp.mean(jnp.square(q - batch.q))
@@ -114,49 +165,58 @@ def main(argv=None):
 
     #     return loss
 
-    minibatch_size = 10
     loss_fn = functools.partial(loss_function, minibatch_size=minibatch_size)
-    # loss_fn = functools.partial(loss_function)
     grad_function = jax.value_and_grad(loss_fn, allow_int=True)
     grad_fn = jax.jit(grad_function)
 
-    num_learning_iterations = 1000
+    num_learning_iterations = 100
     param_history = []
     loss_history = []
     start_time = time.time()
-    for iteration in range(num_learning_iterations):
-        q_train = jnp.roll(q_measured, minibatch_size, axis=0)[:minibatch_size]
-        qd_train = jnp.roll(qd_measured, minibatch_size, axis=0)[:minibatch_size]
-        ctrl = jnp.roll(control_history, minibatch_size, axis=0)[:minibatch_size]
+    for i in range(num_learning_iterations):
+        # Shuffle Data and use same key to preserve order:
+        key, subkey = jax.random.split(subkey)
+        q_train = jax.random.permutation(subkey, q_batch, axis=0)
+        qd_train = jax.random.permutation(subkey, qd_batch, axis=0)
+        ctrl_train = jax.random.permutation(subkey, ctrl_batch, axis=0)
+        for j in range(num_batches):
+            # Get the minibatch:
+            q = q_train[j]
+            qd = qd_train[j]
+            ctrl = jnp.swapaxes(ctrl_train[j], 0, 1)
+            batch = minibatch(
+                q, qd, ctrl,
+            )
+            states = init_fn(
+                sys,
+                batch.q[:, 0],
+                batch.qd[:, 0],
+            )
 
-        batch = minibatch(q_train, qd_train, ctrl)
-        states = init_fn(
-            sys,
-            q_train[0],
-            qd_train[0],
-        )
+            loss, grad = grad_fn(
+                sys, states, batch,
+            )
 
-        loss, grad = grad_fn(
-            sys, states, batch,
-        )
+            # Extract the gradients:
+            friction_gradient = grad.dof_damping
 
-        # Extract the gradients:
-        friction_gradient = grad.dof_damping
+            # Update the parameters:
+            updates, opt_state = solver.update(
+                friction_gradient, opt_state, params,
+            )
+            params = optax.apply_updates(params, updates)
+            params = jnp.clip(params, 0.01, 1.0)
 
-        # Update the parameters:
-        learning_rate = 1e-2
-        gradient_descent = lambda x, dx: x - learning_rate * dx
+            param_history.append(params)
+            loss_history.append(loss)
 
-        friction_param = jax.tree.map(
-            gradient_descent, friction_param, friction_gradient,
-        )
-        param_history.append(friction_param)
-        loss_history.append(loss)
+            # Update the system:
+            sys = sys.replace(
+                dof_damping=params,
+            )
 
-        # Update the system:
-        sys = sys.replace(
-            dof_damping=friction_param,
-        )
+        # Print the loss:
+        print(f'Iteration: {i}, Loss: {loss}')
 
     print(f'Time taken: {time.time() - start_time}')
 
@@ -169,6 +229,18 @@ def main(argv=None):
         q_history.append(state.q)
         qd_history.append(state.qd)
 
+    # Run random trial with regressed params:
+    q_test = q_measured[:, random_idx]
+    qd_test = qd_measured[:, random_idx]
+    ctrl_test = control_history[:, random_idx]
+    state = jax.jit(pipeline.init)(sys, q_test[0], qd_test[0])
+    q_history = []
+    qd_history = []
+    for i in range(control_history.shape[0]):
+        state = jax.jit(pipeline.step)(sys, state, ctrl_test[i])
+        q_history.append(state.q)
+        qd_history.append(state.qd)
+
     param_history = np.asarray(param_history)
     loss_history = np.asarray(loss_history)
     q_history = np.asarray(q_history)
@@ -176,7 +248,7 @@ def main(argv=None):
     q_history_init = np.asarray(q_history_init)
     qd_history_init = np.asarray(qd_history_init)
 
-    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 10))
+    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 5))
     ax[0].plot(param_history[:, 0], color='orange', label='Abduction', linewidth=3.0)
     ax[0].plot(param_history[:, 1], color='cornflowerblue', label='Hip', linewidth=3.0)
     ax[0].plot(param_history[:, 2], color='lightcoral', label='Knee', linewidth=3.0)
@@ -193,12 +265,12 @@ def main(argv=None):
 
     plt.savefig('regress_params.png')
 
-    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 10))
+    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 5))
     fig.suptitle('Regressed Comparison')
 
-    ax[0].plot(q_measured[:, 0], color='orange', linestyle='--', linewidth=3.0)
-    ax[0].plot(q_measured[:, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
-    ax[0].plot(q_measured[:, 2],  color='lightcoral', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 0], color='orange', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 2],  color='lightcoral', linestyle='--', linewidth=3.0)
     ax[0].plot(q_history[:, 0], color='orange', label='Abduction', linewidth=3.0)
     ax[0].plot(q_history[:, 1], color='cornflowerblue', label='Hip', linewidth=3.0)
     ax[0].plot(q_history[:, 2], color='lightcoral', label='Knee', linewidth=3.0)
@@ -206,9 +278,9 @@ def main(argv=None):
     ax[0].set_title('Position')
     ax[0].set_ylabel('Position')
 
-    ax[1].plot(qd_measured[:, 0], color='orange', linestyle='--', linewidth=3.0)
-    ax[1].plot(qd_measured[:, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
-    ax[1].plot(qd_measured[:, 2], color='lightcoral', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 0], color='orange', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 2], color='lightcoral', linestyle='--', linewidth=3.0)
     ax[1].plot(qd_history[:, 0], color='orange', label='Abduction', linewidth=3.0)
     ax[1].plot(qd_history[:, 1], color='cornflowerblue', label='Hip', linewidth=3.0)
     ax[1].plot(qd_history[:, 2], color='lightcoral', label='Knee', linewidth=3.0)
@@ -219,12 +291,12 @@ def main(argv=None):
 
     plt.savefig('regressed.png')
 
-    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 10))
+    fig, ax = plt.subplots(2, 1, constrained_layout=True, figsize=(10, 5))
     fig.suptitle('Initial Comparison')
 
-    ax[0].plot(q_measured[:, 0], color='orange', linestyle='--', linewidth=3.0)
-    ax[0].plot(q_measured[:, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
-    ax[0].plot(q_measured[:, 2],  color='lightcoral', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 0], color='orange', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
+    ax[0].plot(q_measured[:, random_idx, 2],  color='lightcoral', linestyle='--', linewidth=3.0)
     ax[0].plot(q_history_init[:, 0], color='orange', label='Abduction', linewidth=3.0)
     ax[0].plot(q_history_init[:, 1], color='cornflowerblue', label='Hip', linewidth=3.0)
     ax[0].plot(q_history_init[:, 2], color='lightcoral', label='Knee', linewidth=3.0)
@@ -232,9 +304,9 @@ def main(argv=None):
     ax[0].set_title('Position')
     ax[0].set_ylabel('Position')
 
-    ax[1].plot(qd_measured[:, 0], color='orange', linestyle='--', linewidth=3.0)
-    ax[1].plot(qd_measured[:, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
-    ax[1].plot(qd_measured[:, 2], color='lightcoral', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 0], color='orange', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 1], color='cornflowerblue', linestyle='--', linewidth=3.0)
+    ax[1].plot(qd_measured[:, random_idx, 2], color='lightcoral', linestyle='--', linewidth=3.0)
     ax[1].plot(qd_history_init[:, 0], color='orange', label='Abduction', linewidth=3.0)
     ax[1].plot(qd_history_init[:, 1], color='cornflowerblue', label='Hip', linewidth=3.0)
     ax[1].plot(qd_history_init[:, 2], color='lightcoral', label='Knee', linewidth=3.0)
