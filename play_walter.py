204014647e3c13dce0b22f359cf2f176ebc1efd7
jeh15 <jeh15@fsu.edu>
2024-07-25 17:02:36 -0400

    Changed parameters.

--- play_walter.py
+++ play_walter.py
@@ -41,9 +41,8 @@ def main(argv=None):
 
     num_steps = 1000
     states = []
-    rewards = 0
     for i in range(num_steps):
-        state.info['command'] = jnp.array([1.0, 0.0])
+        state.info['command'] = jnp.array([0.0, 0.75])
         key, subkey = jax.random.split(key)
         action, _ = inference_fn(state.obs, subkey)
         state = step_fn(state, action)
